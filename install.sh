2a8dc472ca699dce23d9ef774504121db4ae5db9
MDP9786 <66461507+md9786@users.noreply.github.com>
2025-08-26 01:58:29 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "Installing FRP Web-UI" && mkdir -p /root/frp && cd /root/frp && wget -O frp-ui https://raw.githubusercontent.com/md9786/FRP-Management-Webui/refs/heads/main/frp_ui && chmod +x frp-ui && echo "[Unit]
+echo "Installing FRP Web-UI" && 
+mkdir -p /root/frp && 
+cd /root/frp && 
+wget -O frp-ui https://raw.githubusercontent.com/md9786/FRP-Management-Webui/refs/heads/main/frp_ui && 
+chmod +x frp-ui && 
+wget -O EFRP.sh https://raw.githubusercontent.com/md9786/FRP-Management-Webui/main/Source/EFRP.sh && 
+chmod +x EFRP.sh && 
+echo "[Unit]
 Description=FRP Web UI
 After=network.target
 
@@ -10,4 +17,24 @@ ExecStart=/root/frp/frp-ui
 Restart=always
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/frp_ui.service && systemctl stop frp_ui.service 2>/dev/null || true && systemctl disable frp_ui.service 2>/dev/null || true && systemctl daemon-reload && systemctl enable frp_ui.service && systemctl start frp_ui.service && ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v "127.0.0.1" | head -n 1) && echo "FRP Web-UI installed, you can access it using http://$ip:5001"
+WantedBy=multi-user.target" > /etc/systemd/system/frp_ui.service && 
+echo "[Unit]
+Description=FRP Services Monitor 
+After=network.target frpc@client-42420.service frpc@client-42421.service frpc@client-42422.service frpc@client-42423.service frpc@client-42424.service
+
+[Service]
+ExecStart=/root/frp/EFRP.sh
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/EFRP.service && 
+systemctl stop frp_ui.service 2>/dev/null || true && 
+systemctl disable frp_ui.service 2>/dev/null || true && 
+systemctl daemon-reload && 
+systemctl enable frp_ui.service && 
+systemctl start frp_ui.service && 
+systemctl enable EFRP.service && 
+systemctl start EFRP.service && 
+ip=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v "127.0.0.1" | head -n 1) && 
+echo "FRP Web-UI installed, you can access it using http://$ip:5000"
